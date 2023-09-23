a8f81cd5c9ccf3a9d6b750052f1e7e527bc506c8
tbourgain <thomas.bourgain@gmail.com>
2023-09-23 18:39:28 +0200

    new behavior for combos

--- keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,        KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     SFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,   KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH) ,
-    KC_LCTL,     KC_LGUI, KC_LALT, MO(_LW), KC_SPC, KC_LCTL, KC_RALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT ),
+    KC_LCTL,     KC_LALT, KC_LGUI, MO(_LW), KC_SPC, KC_LCTL, KC_RALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT ),
 
   /*
    *  !       @     up     {    }        ||     pgup    7     8     9    *
@@ -28,8 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RS] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
     KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_COLN,
-    KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
-    QK_LEAD, KC_INS,  KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL, KC_RALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
+    SFT_T(KC_LBRC), KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, RSFT_T(KC_PLUS),
+    QK_LEAD, KC_LALT,  KC_INS , KC_LSFT, KC_BSPC, KC_LCTL, KC_RALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
 
   /*
    * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
@@ -53,10 +53,6 @@ void leader_end_user(void) {
        SEND_STRING("thomas.bourgain@gmail.com");
     } else if (leader_sequence_two_keys(KC_I, KC_M)) {
        SEND_STRING("thomas.bourgain@ing.com");
-    } else if (leader_sequence_two_keys(KC_S, KC_I)) {
-       SEND_STRING("sudo -i -r unconfined_r -t unconfined_t");
-    } else if (leader_sequence_two_keys(KC_N, KC_R)) {
-       SEND_STRING("newrole -r sysadm_r");
     } else if (leader_sequence_two_keys(KC_B, KC_B)) {
        SEND_STRING("INGPROD\tPROD01mgt\n");
     }  
