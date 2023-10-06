b3eeab1e6059a788e9020a56fe8ad754949ce2fe
Thomas Bourgain <thomas.bourgain@gmail.com>
2023-10-06 16:33:45 +0200

    update defines

--- keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
@@ -15,36 +15,32 @@ enum layer_names {
     _MS,
 };
 
+#define SFT_Z  LSFT_T(KC_Z)
+#define SFTSL  RSFT_T(KC_SLSH)
+#define LTLWS  LT(_LW, KC_SPC)
+#define LTRSS  LT(_RS, KC_SPC)
+#define LTMOS  LT(_MS, KC_SPC)
+#define MTAGR  MT(MOD_RALT, KC_SPC)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,        KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-    SFT_T(KC_Z), KC_X,    KC_C,    KC_V,    KC_B,   KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH) ,
-    KC_LCTL,     KC_LGUI, KC_LALT, LT(_LW, KC_SPC), KC_SPC, MO(_MS), KC_RALT, KC_SPC,  LT(_RS, KC_SPC), KC_MINS, KC_QUOT, KC_ENT ),
+    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,                    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,  KC_G,                    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN,
+    SFT_Z,   KC_X,    KC_C,    KC_V,  KC_B,   KC_GRV, KC_BSLS, KC_N,    KC_M,  KC_COMM, KC_DOT,  SFTSL,
+    KC_LCTL, KC_LGUI, KC_LALT, LTLWS, KC_SPC, LTMOS,  MTAGR,   KC_SPC,  LTRSS, KC_MINS, KC_QUOT, KC_ENT ),
 
-  /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
-   */
   [_RS] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
     KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_COLN,
     SFT_T(KC_LBRC), KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, RSFT_T(KC_PPLS),
-    QK_LEAD, KC_LGUI,  KC_LALT , KC_LSFT, KC_SPC, KC_LCTL, KC_RALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
+    QK_LEAD, KC_LGUI,  KC_LALT , KC_LSFT, KC_SPC, KC_LCTL, MTAGR,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
 
-  /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
-   */
   [_LW] = LAYOUT( /* [> LOWER <] */
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11,
     KC_NO,   KC_VOLU, DM_REC1, DM_PLY1,   QK_BOOT, _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12,
-    KC_NO,   KC_VOLD, KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SCRL, DM_RSTP ),
+    KC_NO,   KC_VOLD, KC_LALT, KC_LSFT, KC_BSPC, KC_LCTL, MTAGR, KC_SPC,  TO(_QW), KC_PSCR, KC_SCRL, DM_RSTP ),
 
   [_MS] = LAYOUT( /* [> MOUSE <] */
     _______, _______, _______, _______, _______,                   _______, KC_BTN1, KC_MS_U, KC_BTN2, _______,
