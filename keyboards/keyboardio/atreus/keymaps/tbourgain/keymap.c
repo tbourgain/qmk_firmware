e44de0fc0ab558ed4f08dbb877818b505fdc0faf
tbourgain <thomas.bourgain@gmail.com>
2023-10-24 15:08:33 +0200

    home row mod

--- keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
@@ -21,14 +21,25 @@ enum layer_names {
 #define LTRSS  LT(_RS, KC_SPC)
 #define LTMOS  LT(_MS, KC_SPC)
 #define MTAGR  MT(MOD_RALT, KC_SPC)
+// Left-hand home row mods
+#define HOME_A LSFT_T(KC_A)
+#define HOME_S LCTL_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LALT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J LALT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L RCTL_T(KC_L)
+#define HOME_SCLN RSFT_T(KC_SCLN)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,                    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,  KC_G,                    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN,
-    SFT_Z,   KC_X,    KC_C,    KC_V,  KC_B,   KC_GRV, KC_BSLS, KC_N,    KC_M,  KC_COMM, KC_DOT,  SFTSL,
-    KC_LCTL, KC_LGUI, KC_LALT, LTLWS, KC_SPC, LTMOS,  MTAGR,   KC_SPC,  LTRSS, KC_MINS, KC_QUOT, KC_ENT ),
+    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,                    KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,
+    HOME_A,  HOME_S,  HOME_D,  HOME_F, KC_G,                    KC_H,   HOME_J, HOME_K,  HOME_L,  HOME_SCLN,
+    SFT_Z,   KC_X,    KC_C,    KC_V,   KC_B,   KC_GRV, KC_BSLS, KC_N,   KC_M,   KC_COMM, KC_DOT,  SFTSL,
+    KC_LCTL, KC_LGUI, KC_LALT, LTLWS,  KC_SPC, LTMOS,  MTAGR,   KC_SPC, LTRSS,  KC_MINS, KC_QUOT, KC_ENT ),
 
   [_RS] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
