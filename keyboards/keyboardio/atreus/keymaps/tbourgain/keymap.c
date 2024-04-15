1eb1b15c920f6f4ee63ba213f5b204649e488b38
tbourgain <thomas.bourgain@gmail.com>
2024-04-15 14:28:40 +0200

    update macro

--- keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
@@ -45,13 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
     KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_COLN,
     SFT_T(KC_LBRC), KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, RSFT_T(KC_PPLS),
-    QK_LEAD, KC_LGUI,  KC_LALT , KC_LSFT, KC_SPC, KC_LCTL, MTAGR,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
+    QK_LEAD, KC_LGUI,  KC_LALT , KC_LSFT, KC_SPC, KC_LCTL, _______, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
 
   [_LW] = LAYOUT( /* [> LOWER <] */
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11,
     KC_NO,   KC_VOLU, DM_REC1, DM_PLY1,   QK_BOOT, _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12,
-    KC_NO,   KC_VOLD, KC_LALT, KC_TRNS, KC_BSPC, KC_LCTL, MTAGR, KC_SPC,  TO(_QW), KC_PSCR, KC_SCRL, DM_RSTP ),
+    KC_NO,   KC_VOLD, KC_LALT, KC_TRNS, KC_BSPC, KC_LCTL, _______, KC_SPC,  TO(_QW), KC_PSCR, KC_SCRL, DM_RSTP ),
 
   [_MS] = LAYOUT( /* [> MOUSE <] */
     _______, _______, _______, _______, _______,                   _______, KC_BTN1, KC_MS_U, KC_BTN2, _______,
@@ -68,8 +68,8 @@ void leader_start_user(void) {
 void leader_end_user(void) {
     if (leader_sequence_two_keys(KC_G, KC_M)) {
        SEND_STRING("thomas.bourgain@gmail.com");
-    } else if (leader_sequence_two_keys(KC_I, KC_M)) {
-       SEND_STRING("thomas.bourgain@ing.com");
+    } else if (leader_sequence_two_keys(KC_V, KC_I)) {
+       SEND_STRING("tb@veloc.it");
     }
 }
 
