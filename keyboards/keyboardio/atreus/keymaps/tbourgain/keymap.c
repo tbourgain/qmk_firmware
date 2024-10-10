534ce57f193d40d5dab4b3f196e0175a70c7d2a5
tbourgain <thomas.bourgain@gmail.com>
2024-10-10 14:21:40 +0200

    updates

--- keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/tbourgain/keymap.c
@@ -37,7 +37,7 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,                    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,  KC_G,                    KC_H,   KC_J,  KC_K,    KC_L,    HOME_SCLN,
+    KC_A,    KC_S,    KC_D,    KC_F,  KC_G,                    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN,
     SFT_Z,   KC_X,    KC_C,    KC_V,  KC_B,   KC_GRV, KC_BSLS, KC_N,   KC_M,  KC_COMM, KC_DOT,  SFTSL,
     KC_LCTL, KC_LGUI, KC_LALT, LTLWS, KC_SPC, LTMOS,  MTAGR,   KC_SPC, LTRSS, KC_MINS, KC_QUOT, KC_ENT ),
 
